7ad588d4d6f844043a7df489f209552f2c57ac63
masa_utipinder <remous.progres@gmail.com>
2019-03-29 00:05:08 +0900

    hello worldを追加した。

--- コード.js
+++ コード.js
@@ -1,3 +1,7 @@
 function myFunction() {
-  
+  var selectSpreadSheet = SpreadSheetApp.getActiveSpreadsheet();
+  var sheet = selectSpreadSheet.getSheetByName('シート1');
+  var range = sheet.getRange(1,1,1);
+
+  range.setValue('HelloGASWord');
 }
